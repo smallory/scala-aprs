c3660e002bc10b56da9a1833d7d92edd5ce4401e
smallory <smallory@hcoop.net>
2016-12-07 09:34:53 -0700

    Correcting regex bug

--- src/main/scala/net/hcoop/smallory/scalaprs/AprsDate.scala
+++ src/main/scala/net/hcoop/smallory/scalaprs/AprsDate.scala
@@ -7,6 +7,9 @@
 
 package net.hcoop.smallory.scalaprs
 
+import java.time.ZonedDateTime
+import java.time.format.{DateTimeFormatter, DateTimeParseException}
+
 class AprsDate {
   import net.hcoop.smallory.scalaprs.{AprsDate => our}
   var theDate: String = null
@@ -14,10 +17,20 @@ class AprsDate {
   override def toString(): String = {
     return theDate
   }
+
+  def asDate(): ZonedDateTime = {
+    val fmt = DateTimeFormatter.ofPattern("yyyy MMM dd, kk:mm").withZone(utc)
+    try {
+      return ZonedDateTime.parse(theDate, fmt)
+    } catch {
+      case e: DateTimeParseException => return null
+    }
+    return null
+  }
 }
 object AprsDate {
   val zTimeRegex = """(\d\d)(\d\d)(\d\d)([z/h])""".r
-  val fixedTimeRegex = """\d\d(\d\d)(\d\d)(\d\d)""".r
+  val fixedTimeRegex = """(\d\d)(\d\d)(\d\d)(\d\d)""".r
 
   def apply(payload: String) = {
     val d = new AprsDate()
@@ -37,8 +50,9 @@ object AprsDate {
       case None => {
         fixedTimeRegex findFirstIn payload match {
           case Some(tt) => {
+            println(payload)
             val fixedTimeRegex(d, h, m, s) = tt
-            return s"$year $month $h:$m"
+            return s"$year $month $day $h:$m"
           }
           case None => return  s"$year $month $day, $hour:$minute"
           }
